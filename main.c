7031c469e6e57f6371c819d6c9b616a3e4a99232
Ballexx <alexsenipalu@gmail.com>
2024-01-19 20:33:21 +0100

    new

--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 
 static const double e = 2.7182818;
 
+typedef struct{
+    int IO_count;
+    int hidden_count;
+    int layer_count;
+} Node;
+
+typedef struct{
+    double min;
+    double max;
+} Target;
+
 double* generateWeights(int IO_count, int hidden_count, int layer_count){
     int total_weight_count = 2 * (IO_count * hidden_count) + ((hidden_count * hidden_count) * (layer_count-1));
 
@@ -33,18 +44,40 @@ double* generateBias(int layer_count){
     return biases;
 }
 
+double* generateTargets(int IO_count, Target *target){ 
+    double* targets = malloc(IO_count * (sizeof(double)));
+    targets[0] = target->min;
+
+    double interval = 0;
+
+    if(IO_count % 2 == 0){
+        interval = (double)1 / (IO_count-1);
+    }
+    else{
+        interval = (double)1 / (IO_count);
+    }
+
+    if(IO_count > 2){
+        for(int i = 1; i < IO_count; i++){
+            targets[i] = interval;
+            interval += interval;
+        }
+    }
+
+    targets[IO_count-1] = target->max;
+
+    for(int i = 0; i < IO_count; i++){
+        printf("%f\n", targets[i]);
+    }
+
+    return targets;
+}
+
 double sigmoid(double n){
     return 1 / (1 + (1 / powf(e, n)));
 }
 
-double* feedForward(
-    int pre_node_count, 
-    int post_node_count,
-    double weights[],
-    double bias,
-    double input[]
-    ){
-
+double* calculateHidden(int pre_node_count, int post_node_count, double weights[], double bias, double inputs[]){
     double* result = malloc(post_node_count * sizeof(double));
 
     int current_weight = 0;
@@ -53,7 +86,7 @@ double* feedForward(
         double new_value = 0;
 
         for(int j = 0; j < pre_node_count; j++){
-            new_value = new_value + (input[j] * weights[current_weight]);
+            new_value = new_value + (inputs[j] * weights[current_weight]);
             current_weight++;
         }
         result[i] = sigmoid(new_value + bias);
@@ -63,30 +96,22 @@ double* feedForward(
     return result;    
 }
 
-typedef struct{
-    int IO_count;
-    int hidden_count;
-    int layer_count;
-} Node;
-
-typedef struct{
-    double min;
-    double max;
-} Target;
+double* feedForward(Node *node, double weights[], double biases[], double inputs[]){
+}
 
 int main(){
     srand(time(NULL));
 
     Node node;
 
-    node.IO_count = 2;
+    node.IO_count = 4;
     node.hidden_count = 3;
     node.layer_count = 1;
 
-    double* input = malloc(node.IO_count * (sizeof(double)));
+    double* inputs = malloc(node.IO_count * (sizeof(double)));
 
-    input[0] = 0.05;
-    input[1] = 0.10;
+    inputs[0] = 0.05;
+    inputs[1] = 0.10;
 
     double* weights = generateWeights(
         node.IO_count,
@@ -98,10 +123,12 @@ int main(){
 
     Target target;
 
-    target.max = 0.99;
     target.min = 0.01;
+    target.max = 0.99;
+    
+    double* targets = generateTargets(node.IO_count, &target);
 
-    double* new_value = feedForward(node.IO_count, node.hidden_count, weights, biases[0], input);
+    double* new_value = feedForward(&node, weights, biases, inputs);
 }
 
 //https://www.javatpoint.com/pytorch-backpropagation-process-in-deep-neural-network
